56a2eafc4a554b24564a05e1bddba0cde91669e4
dabutchart-sudo <dabutchart@googlemail.com>
2025-11-29 14:30:40 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 export const SUPABASE_URL = "https://dntitlrtvkgisxwqjxch.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRudGl0bHJ0dmtnaXN4d3FqeGNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxOTQ0MzEsImV4cCI6MjA3OTc3MDQzMX0.S9CXEsHoqp9ATaX23nLI77Q78SvVUSp9V30U-MNcm90";
-export const APP_VERSION = "1.9";
+export const APP_VERSION = "1.1";
+
 
 
 
