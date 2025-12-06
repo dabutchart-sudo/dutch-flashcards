8abe3db1bb28cf02ecf340ace17ce5a8a4c6d6bd
dabutchart-sudo <dabutchart@googlemail.com>
2025-12-06 17:30:11 +0000

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -9,7 +9,7 @@ export const SUPABASE_URL = "https://dntitlrtvkgisxwqjxch.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRudGl0bHJ0dmtnaXN4d3FqeGNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxOTQ0MzEsImV4cCI6MjA3OTc3MDQzMX0.S9CXEsHoqp9ATaX23nLI77Q78SvVUSp9V30U-MNcm90";
 
 // Unsplash Access Key (Client-side is OK for this project)
-export const UNSPLASH_ACCESS_KEY = "YOUR_UNSPLASH_ACCESS_KEY";
+export const UNSPLASH_ACCESS_KEY = "roF6le_ubyOne6ys-UrkyHpl0afaLEvVeiNOq9ifsnM";
 
 // Daily max new card config key
 export const CONFIG_MAX_NEW = "dfc_max_new";
